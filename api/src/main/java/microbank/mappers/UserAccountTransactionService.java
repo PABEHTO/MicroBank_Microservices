d9b82bab658f2b4b5872154d3f2fc2a20cbc05b7
PABEHTO <nyakushoku@gmail.com>
2025-10-25 22:16:16 +1000

    Dockerization + try to documentation by springdoc OpenAPI (Swagger)

--- api/src/main/java/microbank/mappers/UserAccountTransactionService.java
+++ api/src/main/java/microbank/mappers/UserAccountTransactionService.java
@@ -1,9 +1,24 @@
 package microbank.mappers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "UserComposite", description = "some information...")
 public interface UserAccountTransactionService {
+    @Operation(
+            summary = "${api.user-composite.get-composite-user.description}",
+            description = "${api.user-composite.get-composite-user.notes}"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @GetMapping(
             value = "/composite",
             produces = "application/json"
--- microservices/composite-user-account-transaction/src/main/java/microbank/core/composite/services/UserAccountTransactionServiceImpl.java
+++ microservices/composite-user-account-transaction/src/main/java/microbank/core/composite/services/UserAccountTransactionServiceImpl.java
@@ -4,6 +4,7 @@ import microbank.core.account.Account;
 import microbank.core.transaction.Transaction;
 import microbank.mappers.UserAccountTransaction;
 import microbank.mappers.UserAccountTransactionService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,18 @@ import java.util.List;
 @RestController
 public class UserAccountTransactionServiceImpl implements UserAccountTransactionService {
     private final RestTemplate restTemplate;
+    /*@Value("${app.user-service.host}")
+    private String userServiceHost;
+    @Value("${app.user-service.port}")
+    private String userServicePort;*/
+    @Value("${app.account-service.host}")
+    private String accountServiceHost;
+    @Value("${app.account-service.port}")
+    private String accountServicePort;
+    @Value("${app.transaction-service.host}")
+    private String transactionServiceHost;
+    @Value("${app.transaction-service.port}")
+    private String transactionServicePort;
 
     public UserAccountTransactionServiceImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -21,12 +34,13 @@ public class UserAccountTransactionServiceImpl implements UserAccountTransaction
 
     @Override
     public UserAccountTransaction getUserAccountTransaction(int userId) {
-        Account account = restTemplate.getForObject("http://localhost:7002/account?userId="+userId, Account.class);
+        Account account = restTemplate.getForObject("http://" + accountServiceHost + ":" + accountServicePort + "/account?userId=" + userId, Account.class);
         List<Transaction> transactions = restTemplate.exchange(
-                "http://localhost:7003/transaction?userId=" + userId,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Transaction>>(){})
+                        "http://" + transactionServiceHost + ":" + transactionServicePort + "/transaction?userId=" + userId,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<Transaction>>() {
+                        })
                 .getBody();
 
         return new UserAccountTransaction(userId, "Testy", account, transactions);
